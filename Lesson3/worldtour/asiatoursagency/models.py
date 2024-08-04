5c2cf9faf03ebebca522274ee8c07a78489ad042
Stefan Lauritsen <stefan.lauritsen@gmail.com>
2024-08-05 01:13:36 +0200

    Lesson 3: added model

--- Lesson3/worldtour/asiatoursagency/models.py
+++ Lesson3/worldtour/asiatoursagency/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    # We need a origin country, we need the destination,
+    # number of nights, and we need the price for that tour
+
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nigths = models.IntegerField()
+    price = models.IntegerField()
